c1a8e308f2fea9c43ddbd303dddc2dd79bae6320
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-01-19 19:43:15 +0100

    test popup on click

--- js_dev/grayscale.js
+++ js_dev/grayscale.js
@@ -93,6 +93,11 @@ $(function()
   });
 });
 
+function myFunction() {
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+}
+
 /////    /////    /////    /////
 /////    /////    /////    /////
 /////    /////    /////    /////
